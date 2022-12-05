d2a43ba74b194f657231367de36519b25de57d4f
zatrimaylova <m.zatrimaylova@gmail.com>
2022-12-05 21:40:52 +0300

    added changes to fs/copy.js and fs/create.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,43 @@
+const fs = require('fs');
+
+const folderToCopy = './files';
+const copiedFolder = './files_copy';
+
 const copy = async () => {
-    // Write your code here 
+    fs.access(folderToCopy, fs.F_OK, (err) => {
+        //if file folder does not exist
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+    })
+
+    fs.access(copiedFolder, fs.F_OK, async (err) => {
+        //if files_copy folder does not exist
+        if (err) {
+            try {
+                await fs.mkdir(copiedFolder, (err) => {
+                    if(err) throw err; // не удалось создать папку
+                });
+
+                await fs.readdir(folderToCopy, (err, files) => {
+                    if(err) throw err;
+ 
+                    files.forEach(file => copyFile(file));
+                });
+            } catch (err) {
+                console.error(err)
+            }
+        } else {
+            throw new Error('FS operation failed');
+        }
+    })
 };
 
-await copy();
+const copyFile = (path) => {
+    const { COPYFILE_EXCL } = fs.constants;
+    fs.copyFile(`${folderToCopy}/${path}`, `${copiedFolder}/${path}`, COPYFILE_EXCL, err => {
+        if(err) throw err;
+    });
+};
+
+copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+const fs = require('fs');
+
+const path = './files/fresh.txt';
+
 const create = async () => {
-    // Write your code here 
+    fs.access(path, fs.F_OK, (err) => {
+        if (err) {
+            fs.writeFile(path, 'I am fresh and young', (error) => {
+                error ? console.log(error) : null;
+            });
+            return;
+        }
+      
+        throw new Error('FS operation failed');
+    })
 };
 
-await create();
+create();
